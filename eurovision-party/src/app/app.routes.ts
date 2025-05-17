8f2948c92f9df80268341b71b24ff048d7c546b8
mariusz <mariuszbozek@fnnek.com>
2025-05-17 16:53:14 +0200

    add backend

--- eurovision-party/src/app/app.routes.ts
+++ eurovision-party/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { NameFormComponent } from './name-form/name-form.component';
 import { DragDroplistComponent } from './dragdroplist.component';
 import {ResultsComponent} from './results/results.component';
 import { HttpClientModule } from '@angular/common/http';
+import {ResultsIndividualComponent} from './results-individual/results-individual.component';
 
 export const routes: Routes = [
   { path: 'start', component: NameFormComponent },
   { path: 'vote/:name', component: DragDroplistComponent },
   { path: 'results', component: ResultsComponent },
-  { path: 'user/results/:id', component: ResultsComponent },
+  { path: 'user/results/:id', component: ResultsIndividualComponent },
   { path: '**', redirectTo: 'start' } // opcjonalne: przekierowanie na /start, jeśli ścieżka nie istnieje
 ];
 
